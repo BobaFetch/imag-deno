c7d7dc038a639c201da539ea02a0e256fdf7e0e7
Andrew D. Riggs <ariggs@imagineticsllc.com>
2022-06-28 21:32:55 -0700

    cleaning up time conversion

--- routes/dept/[dept].tsx
+++ routes/dept/[dept].tsx
@@ -4,22 +4,21 @@ import { Handlers, PageProps } from '$fresh/server.ts'
 import { tw } from '@twind'
 
 import Navigation from '../../islands/Nav.tsx'
-// import deptartmentData from '../../static/deptartment-data';
 
 interface JobList {
-  partRef: string;
-  partNumber: string;
-  run: string;
-  queueDiff: number;
-  qty: number;
-  customer: string;
-  schedDate: string;
-  priority: number;
-  workcenterName: string;
-  comments: string;
-  workcenterNumber: string;
-  purchaseOrder: string;
-  lineItem: string;
+  Part_Ref: string;
+  Part_Num: string;
+  Run: string;
+  Queue_Diff: number;
+  Qty: number;
+  Customer: string;
+  Cust_Date: string;
+  Priority: number;
+  WC_Name: string;
+  Comments: string;
+  WC_Num: string;
+  PO: string;
+  Item: string;
 }
 
 const api = 'http://imawebrpt.imagineticsinc.local:4004/api'
@@ -99,7 +98,7 @@ function Table({jobs}) {
             <tr>
               <td class={tw`border border-black px-2 py-1`}>{job.Part_Num}</td>
               <td class={tw`border border-black px-2 py-1 text-center`}>{job.Run}</td>
-              <td class={tw`border border-black px-2 py-1 text-center`}>{job.Queue_Diff}</td>
+              <td class={tw`border border-black px-2 py-1 text-center`}>{handleTimeDifference(job.Queue_Diff)}</td>
               <td class={tw`border border-black px-2 py-1 text-center`}>{job.Qty}</td>
               <td class={tw`border border-black px-2 py-1 text-center`}>{job.Customer}</td>
               <td class={tw`border border-black px-2 py-1 text-center`}>{new Date(job.Cust_Date).toLocaleDateString()}</td>
@@ -113,132 +112,21 @@ function Table({jobs}) {
   )
 }
 
+function handleTimeDifference(timeMS) {
+  const days = Math.floor(timeMS / 1440);
+  const hours = Math.floor(timeMS / 60);
+  const mins = timeMS % 60;
+  let diff = "";
 
-
-// figure out a better idea for this shit
-const departmentData = [
-  {
-    parent: "Assembly",
-    children: [
-      {
-        name: "Stores ID & Issue",
-        wcNumber: "0301"
-      },
-      {
-        name: "Assembly ID & Issue",
-        wcNumber: "0302"
-      },
-      {
-        name: "Assembly",
-        wcNumber: "2200"
-      },
-      {
-        name: "Assembly Countersink",
-        wcNumber: "2650"
-      },
-      {
-        name: "Part Mark Overcoat",
-        wcNumber: "5075"
-      },
-    ]
-  },
-  {
-    parent: "Sheet Metal",
-    children: [
-      {
-        name: "ID & Issue | Saw",
-        wcNumber: "0300/0500"
-      },
-      {
-        name: "Shear | Router",
-        wcNumber: "0401/1400"
-      },
-      {
-        name: "CNC Punch | AMADA 2510NT",
-        wcNumber: "0700/0750"
-      },
-      {
-        name: "Punch Press | Hydroform",
-        wcNumber: "0900/1000"
-      },
-      {
-        name: "Hot Joggle",
-        wcNumber: '0950'
-      },
-      {
-        name: "Roll Form",
-        wcNumber: "1100"
-      },
-      {
-        name: "Notcher",
-        wcNumber: "1200"
-      },
-      {
-        name: "Deburr",
-        wcNumber: "1600"
-      },
-      {
-        name: "Abrasive Blast",
-        wcNumber: "5600"
-      },
-      {
-        name: "Brake Press",
-        wcNumber: "1700"
-      },
-      {
-        name: "Straightening",
-        wcNumber: "1800"
-      }
-    ]
-  },
-  {
-    parent: "Inspection",
-    children: [
-      {
-        name: "Insp Sheet Metal",
-        wcNumber: "2000"
-      },
-      {
-        name: "Insp CNC",
-        wcNumber: "2005"
-      },
-      {
-        name: "Insp Heat Treat",
-        wcNumber: "2020"
-      },
-      {
-        name: "Insp Finish",
-        wcNumber: "2040"
-      },
-      {
-        name: "Insp Prime | Topcoat",
-        wcNumber: "5030/5035"
-      },
-      {
-        name: "IMAG Cert",
-        wcNumber: "5099"
-      },
-      {
-        name: "Insp Assembly",
-        wcNumber: "2050"
-      },
-      {
-        name: "Insp Final",
-        wcNumber: "2060"
-      }
-    ]
-  },
-  {
-    parent: "Shipping",
-    children: [
-      {
-        name: "Part Mark",
-        wcNumber: "2300"
-      },
-      {
-        name: "Packaging",
-        wcNumber: "2400"
-      }
-    ]
+  if (days > 0) {
+    diff += days === 1 ? `${days} day` : `${days} days`;
+    return diff
+  } else if (hours > 0) {
+    diff += hours === 1 ? `${hours} hour` : `${hours} hours`;
+    return diff
   }
-]
+  return mins === 0 ? `${mins} minute` : `${mins} minutes`
+}
+
+
+
